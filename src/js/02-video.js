a2db6f085c74ac19f739ee93160f668b30670772
NZAlona <amakoveychenko@gmail.com>
2022-12-17 06:11:53 +0200

    Adds task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const videoTime = data.seconds;
+
+    localStorage.setItem('videoplayer-current-time', videoTime);
+  }, 1000)
+);
